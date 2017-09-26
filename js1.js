a68beba3812b2109fe4b10f76c37a571fa2b522c
islf14 <isaiaslflores@gmail.com>
2017-09-26 15:44:03 -0500

    actu

--- js1.js
+++ js1.js
@@ -1,5 +1,19 @@
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
 ctx.fillStyle = "red";
-ctx.fillRect(10, 10, 13, 12);
+ctx.fillRect(10, 10, 10, 10);
 
+function pixel(){
+    ctx.fillStyle ="blue";
+    ctx.fillRect(30, 30, 10, 10);
+}
+
+function copy() {
+    var imgData = ctx.getImageData(10, 10, 10, 10);
+    ctx.putImageData(imgData, 20, 70);
+}
+
+function agrandar() {
+    var imgData = ctx.getImageData(10, 10, 15, 15);
+    ctx.putImageData(imgData, 50, 70);
+}
