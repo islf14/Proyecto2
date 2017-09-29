c3e1e0d2f61ad2bc36f886256fd0e491c75594f7
islf14 <isaiaslflores@gmail.com>
2017-09-28 20:17:16 -0500

    hoy 28 de lap

--- js1.js
+++ js1.js
@@ -1,18 +1,69 @@
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
-ctx.fillStyle = "red";
-ctx.fillRect(10, 10, 10, 10);
+//ctx.fillStyle = "red";
+//ctx.fillRect(10, 10, 10, 10);
 
-function pixel(){
-    var a=document.getElementById("ejex").value;
+function pixel() {
+    var a = document.getElementById("ejex").value;
     var b = document.getElementById("ejey").value;
-    //alert("es es "+a);    
-    if(a=="hola"){
-        alert("es hola");        
+    //alert("a es: " + a);
+    //if(a=="hola"){alert("es hola");}
+    ctx.fillStyle = "blue";
+    ctx.fillRect(a, b, 10, 10);
+}
+
+function redondeo2decimales(numero)
+{
+    var flotante = parseFloat(numero);
+    var resultado = Math.round(flotante*100)/100;
+    console.log("resultado "+ resultado);
+    return resultado;
+}
+
+function linea() {
+    var a = document.getElementById("ejex").value;
+    console.log("recibiendo " + a);
+    var b = document.getElementById("ejey").value;
+    var c = document.getElementById("ejexf").value;
+    var d = document.getElementById("ejeyf").value;
+	
+	if(a==""){
+        alert("Ingrse X0");
+		return false;
+    }
+    if(b==""){
+		alert("Ingrese Y0");
+		return false;
+    }
+    if(c==""){
+		alert("Ingrese Xf");
+		return false;
+    }
+    if(d==""){
+		alert("Ingrese Yf");
+		return false;
     }
     
-    ctx.fillStyle ="blue";
-    ctx.fillRect(a, b, 10, 10);
+    var x0 = Math.round(a);
+	var y0 = Math.round(b);
+	var xf = Math.round(c);
+    var yf = Math.round(d);
+    console.log(x0+ " "+y0+" "+xf+" "+yf);
+		
+    if (x0 < xf) {
+        alert("x0 esta a la izquierda");
+		if (y0==yf) {
+			for(i=x0;i<=xf;i++){
+				ctx.fillStyle = "blue";
+				ctx.fillRect(i, yf, 1, 1);
+			}
+		}
+		
+    }
+	else {
+		alert("x0 es mayor");
+	}
+    
 }
 
 function copy() {
@@ -21,6 +72,6 @@ function copy() {
 }
 
 function agrandar() {
-    var imgData = ctx.getImageData(10, 10, 15, 15);
+    var imgData =ctx.getImageData(10, 10, 15, 15);
     ctx.putImageData(imgData, 50, 70);
 }
