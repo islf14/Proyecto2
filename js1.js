f5da1ea878ab0dcf0b3adb0546a64c390193e185
islf14 <isaiaslflores@gmail.com>
2017-09-29 19:25:36 -0500

    act 29

--- js1.js
+++ js1.js
@@ -12,17 +12,8 @@ function pixel() {
     ctx.fillRect(a, b, 10, 10);
 }
 
-function redondeo2decimales(numero)
-{
-    var flotante = parseFloat(numero);
-    var resultado = Math.round(flotante*100)/100;
-    console.log("resultado "+ resultado);
-    return resultado;
-}
-
 function linea() {
     var a = document.getElementById("ejex").value;
-    console.log("recibiendo " + a);
     var b = document.getElementById("ejey").value;
     var c = document.getElementById("ejexf").value;
     var d = document.getElementById("ejeyf").value;
@@ -49,29 +40,93 @@ function linea() {
 	var xf = Math.round(c);
     var yf = Math.round(d);
     console.log(x0+ " "+y0+" "+xf+" "+yf);
-		
-    if (x0 < xf) {
-        alert("x0 esta a la izquierda");
-		if (y0==yf) {
-			for(i=x0;i<=xf;i++){
-				ctx.fillStyle = "blue";
-				ctx.fillRect(i, yf, 1, 1);
-			}
-		}
-		
+	
+    if (x0 <= xf) {
+        console.log("corrd in: ("+x0+";"+y0+") coord f: ("+xf+";"+yf+")");
+    }else {
+        var auxx = x0;
+        x0 = xf;
+        xf = auxx;
+        var auxy = y0;
+        y0 = yf;
+        yf = auxy;
+        console.log("nuevas corrd in: ("+x0+";"+y0+")  coord f: ("+xf+";"+yf+")");
     }
-	else {
-		alert("x0 es mayor");
-	}
+    var i = 0;
+
+    if (y0==yf) {
+        //grafica una linea horizontal
+        //alert("entrando a graficar línea");
+        for(i=x0;i<=xf;i++){
+            ctx.fillStyle = "blue";
+            ctx.fillRect(i, y0, 1, 1);
+        }
+    }
     
+    if(x0==xf) {
+        //grafica linea vertical
+        for(i=y0;i<=yf;i++){
+            ctx.fillStyle = "blue";
+            ctx.fillRect(x0, i, 1, 1);
+        }
+    }
+    
+    //hallando  diferenciales y pendiente
+    var dy = yf-y0;
+    var dx = xf-x0;
+    console.log("dy: "+dy+" dx: "+dx);
+    var pendiente = 0;
+    if(dy==dx){
+        pendiente = 1;
+    }
+    var auxpend = dx*(-1);
+    console.log("dx*-1 = "+auxpend);
+    if(dy==auxpend){
+        pendiente = -1;
+    }
+    console.log("pendiente: " + pendiente);
+    
+    //dibujando line diagonal 45
+    var x=x0;
+    var y=y0;
+    i=0;
+    if(pendiente==1){
+        while ((x+i)<=xf){
+            ctx.fillStyle = "blue";
+            ctx.fillRect(x+i, y+i, 1, 1);
+            i=i+1;
+        }
+        console.log("i en m=1: "+i);
+        
+    }
+    if(pendiente==-1){
+        while ((x+i)<=xf){
+            ctx.fillStyle = "blue";
+            ctx.fillRect(x+i, y-i, 1, 1);
+            i=i+1;
+        }
+        console.log("i en m=-1: "+i);        
+    }
+
+    //Dibuajando linea arbitraria
+    y=y0;
+    x=x0;
+    pendiente = dy/dx;
+    console.log("pendiente en arbitraria"+pendiente)
+    b=y-pendiente*x;
+    console.log("b: "+b);
+    ctx.fillStyle = "blue";
+    ctx.fillRect(x, y, 1, 1);
+    for(i=x+1;i<=xf;i++) {
+        y=Math.round(pendiente*i+b);
+        console.log("y:"+y)
+        ctx.fillStyle = "blue";
+        ctx.fillRect(i, y, 1, 1);
+    }
 }
 
+/*
 function copy() {
     var imgData = ctx.getImageData(10, 10, 10, 10);
     ctx.putImageData(imgData, 20, 70);
-}
-
-function agrandar() {
-    var imgData =ctx.getImageData(10, 10, 15, 15);
-    ctx.putImageData(imgData, 50, 70);
-}
+}*/
