c794e0e40c7fa6efb68013e9bd34035c78719223
islf14 <isaiaslflores@gmail.com>
2017-12-19 16:36:41 -0500

    probando mouse

--- js/lienzo.js
+++ js/lienzo.js
@@ -1,9 +1,11 @@
-var x=0,y=0;
+var x=0,y=0,valorx=null,valory=null;
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
 var a;
 var cont_puntos=0;
-
+//var ejex = document.getElementById("ejex");
+//var ejey = document.getElementById("ejey");
+var campo1,campo2,indicador=false;
 function en_lienzo(){
     event = event || window.event;
     x = event.pageX - c.offsetLeft,
@@ -14,38 +16,62 @@ function en_lienzo(){
     console.log("x: "+x);
     console.log("y: "+y);    
 }
+
+document.getElementById("ejex").addEventListener("click", function(){
+    asignarxy();
+});
+document.getElementById("ejey").addEventListener("click", function(){
+    asignarxy();
+});
+
+document.getElementById("ejexf").addEventListener("click", function(){
+    asignarxyf();
+});
+document.getElementById("ejeyf").addEventListener("click", function(){
+    asignarxyf();
+});
+function asignarxy(){
+    campo1 = document.getElementById("ejex");
+    campo2 = document.getElementById("ejey");
+    activamouse(campo1,campo2);
+    indicador=true;
+}
+function asignarxyf(){
+    campo1 = document.getElementById("ejexf");
+    campo2 = document.getElementById("ejeyf");
+    activamouse(campo1,campo2);
+    indicador=true;
+}
+
+function activamouse(cm1,cm2) {
+    c.addEventListener('mousemove', function(evt){
+        var mouse_pos = posicion(c,evt);
+        var a = mouse_pos.x;
+        var b = mouse_pos.y;
+        cm1.value = a;
+        cm2.value = b;
+    });
+}
 function posicion(canvas,evt,x,y) {
     var rect = canvas.getBoundingClientRect();
+    valorx = parseInt(evt.clientX - rect.left);
+    valory = parseInt((rect.top+canvas.height)-evt.clientY);
     return {
         x: parseInt(evt.clientX - rect.left),
         y: parseInt((rect.top+canvas.height)-evt.clientY)
     };
 }
-
 c.addEventListener("click", function(evt){
     cont_puntos++;
-    console.log(cont_puntos);
-    //document.getElementById("demo").innerHTML = "Hello World!";
+    console.log("cont_puntos: "+cont_puntos);
+    var a = valorx;//mouse_pos.x;
+    var b = valory;//mouse_pos.y;
+    if(indicador==true){
+        campo1.value = a;
+        campo2.value = b;
+        c.addEventListener("mousemove",function(evt){
+            campo1.value = a;
+            campo2.value = b;
+        });
+    } 
 });
-c.addEventListener('mousemove', function(evt) {
-    var mouse_pos = posicion(c,evt);
-    var a = mouse_pos.x;
-    var b = mouse_pos.y;
-    var ejex = document.getElementById("ejex");
-    var ejey = document.getElementById("ejey");
-    ejex.value = a;
-    ejey.value = b;
-
-    /*if (band == 0) {
-      var msg_x = mousePos.x;
-      var msg_y =  mousePos.y;
-      if(md_numx1 != null && md_numy1 != null)
-      {
-        md_numx1.value = msg_x;
-        md_numy1.value= msg_y;
-      }
-      //debugger;
-    }
-    */
-
-  });
